a0338d6a9ad280f17a23c4204551fdaae89a1115
familylappy <familylappy@10.211.55.3>
2020-03-23 16:53:31 -0500

    hi commiot

--- src/Multithreded/MyMultithreading.java
+++ src/Multithreded/MyMultithreading.java
@@ -13,11 +13,26 @@ public class MyMultithreading {
 		//5.Synchronization
 		//6.Inter-thread communication
 		//7.deadlock
-		//8.deamon thread
-		//9.multithreading enhancements
+		//8.Daemon thread
+		//9.Multithreading enhancements
 		
 		
-
+		//Executing several tasks simultaneously called concept of multitasking.
+		//there are two types of multitasking
+		//1.ProcessBased multitasking and 2.thread based multitasking
+		
+		//Process based multitasking :Executing several tasks simultaneously where every task is 
+		//separate independent program(process) is called process based multitasking..OS level multitasking
+		//e.g while typing java program we can listen music from same system at the same time we can download a file 
+		//net all these tasks will be executed simultaneously and independent of each other.
+		
+		//Thread based multitasking: Executing several tasks simultaneously where each task is a separate independent 
+		//part of the same program is called as thread based multitasking.
+		
+		//objective of multitasking is to reduce response time of system to improve performance.
+		
+		//
+            
 	}
 
 }
